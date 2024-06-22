b2744531bea51eb294ddb228a8d053ff1a99f90d
manh <truongducmanh07092004@gmail.com>
2024-06-22 22:06:28 +0700

    init-project

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,27 @@
 const initialState = {
     users: [
-        { id: 1, name: 'Manh' }
+        { id: 1, name: 'Manh' },
+        { id: 2, name: 'Manh2' }
+
     ]
 }
 const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'Delete User':
+            console.log(action);
+            let users = state.users;
+            users = users.filter((item => item.id !== action.payload.id));
+            return {
+                ...state, users
+            };
+        case 'Create User':
+            console.log(action);
+            let id = Math.floor(Math.random() * 10000)
+            let user = { id: id, name: `random - ${id}` }
+            return {
+                state, users: [...state.users, user]
+            }
+    }
     return state;
 }
 
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,13 +1,43 @@
 import React from "react";
 import Color from "../HOC/Color";
 import { connect } from "react-redux";
+import { type } from "@testing-library/user-event/dist/type";
 // HOC higher order component
 class Home extends React.Component {
 
 
+    handleDeleteUser = (user) => {
+        console.log(user.name);
+        this.props.deleteUserRedux(user);
+    }
+
+    handleAddUser = () => {
+        this.props.addUserRedux();
+    }
     render() {
+        let listUsers = this.props.dataRedux;
         return (
-            <div>Hello</div>
+            <>
+                <div>
+                </div>
+                <div>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                // <>
+                                <div key={item.id}>
+                                    {item.id} - {item.name}
+                                    <span>   </span>
+                                    <span onClick={() => this.handleDeleteUser(item)}>X</span>
+                                </div>
+                                // </>
+                            )
+                        })
+                    }
+                </div>
+                <div onClick={() => this.handleAddUser()}>Add New
+                </div>
+            </>
         )
     }
 }
@@ -15,5 +45,22 @@ class Home extends React.Component {
 const mapStateToProps = (state) => {
     return { dataRedux: state.users }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) => {
+            dispatch({
+                type: 'Delete User',
+                payload: userDelete
+            })
+        },
+
+        addUserRedux: () => {
+            dispatch({
+                type: 'Create User'
+            })
+        }
+    }
+}
 // export default withRouter(Home);
-export default connect(mapStateToProps)(Color(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(Color(Home));
